0a8e17371acfb41567a3ffc038cf1ad7052f1d79
Kastilhos <g.kastilhos@gmail.com>
2022-01-14 00:53:33 -0300

    correction to formatOutput

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
         for(var i = 0; i < res.items.length; i+=2) {
             item = res.items[i];
             title1 = item.volumeInfo.title;
-            author1 = item.volumeInfo.author;
+            author1 = item.volumeInfo.authors;
             publisher1 = item.volumeInfo.publisher;
             bookLink1 = item.volumeInfo.previewLink;
             bookIsbn1 = item.volumeInfo.industryIdentifiers[1].identifier
@@ -45,20 +45,20 @@ $(document).ready(function() {
             
             item2 = res.items[i+1];
             title2 = item2.volumeInfo.title;
-            author2 = item2.volumeInfo.author;
+            author2 = item2.volumeInfo.authors;
             publisher2 = item2.volumeInfo.publisher;
             bookLink2 = item2.volumeInfo.previewLink;
             bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
             bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHolder;
         
             outputList.innerHTML += '<div class="row mt-4">' +
-                                    formatOutput(bookImg1, title1, author1, publisher1,bookLink1, bookIsbn1, bookImg1)+
-                                    formatOutput(bookImg2, title2, author2, publisher2,bookLink2, bookIsbn2, bookImg2)+
+                                    formatOutput(bookImg1, title1, authors1, publisher1,bookLink1, bookIsbn1, bookImg1)+
+                                    formatOutput(bookImg2, title2, authors2, publisher2,bookLink2, bookIsbn2, bookImg2)+
                                     '</div>'
 
         }
     }
-    function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+    function formatOutput(bookImg, title, authors, publisher, bookLink, bookIsbn) {
         let viewerUrl = 'book.html?isbn' +bookIsbn;
         let htmlCard = `<div class="col-lg-6">
                         <div class="card" style="">
